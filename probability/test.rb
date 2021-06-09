b46a82103e354f7bf70628bb9277276003eabf7f
angellips <85476798+angellips@users.noreply.github.com>
2021-06-09 07:17:48 -0700

    updated user input handling

--- probability/test.rb
+++ probability/test.rb
@@ -1,3 +1,7 @@
+require 'colorize'
+
+#simplify this entire section
+
 def user_input
 
 puts "Please enter quantity of each color"
@@ -19,21 +23,13 @@ puts "Green"
 
 @user_total = [@user_grey + @user_yellow + @user_blue + @user_red + @user_green]
 
+@user_int = @user_total.inject(0){|sum,x| sum + x }
 
-#test output
-puts @user_total
-
-
-if @user_total > 5
-  then puts "Total quantity can not be greater than five."
+if @user_int != 5
+  then puts "\nERROR: The inputted quantity must be exactly five. Correct your input and try again. \n ".red
   user_input
-elsif @user_total < '5'
-  then puts "Total quantity can not be less than five."
-  user_input
+  #else overall
 end
-
-puts "test"
-puts @user_total
 
 #.to_i.reduce(0, :+)
 
@@ -46,7 +42,7 @@ def overall
 # presuming there are 64 total tapes which would span eight releases
 # independent probability that a run contains a certain color
 
-@overall_perc = [(54.0/64.0)*100, (4.0/64.0)*100, (1.0/64.0)*100, (2.0/64.0)*100, (3.0/64.0)*100]
+@overall_perc = [(26.0/40.0)*100, (4.0/40.0)*100, (1.0/40.0)*100, (2.0/40.0)*100, (3.0/40.0)*100]
 @grey_perc = @overall_perc[0].truncate(2)
 @yellow_perc = @overall_perc[1].truncate(2)
 @blue_perc = @overall_perc[2].truncate(2)
@@ -58,7 +54,7 @@ puts @overall_perc
 
 #total number of each color
 
-@grey_total = 54
+@grey_total = 26
 @yellow_total = 4
 @blue_total = 1
 @red_total = 2
@@ -79,7 +75,30 @@ def prob_input
 # 1/64 * 1/63 * 1/62 * 1/61 * 1/60 = probability of set
 
 
+@grey_input = 1
+@yellow_input = 1
+@blue_input = 1
+@red_input = 1
+@green_input = 1
+
+@overall_input = [@grey_input + @yellow_input + @blue_input + @red_input + @green_input]
+
+@overall_int = @overall_input.inject(0){|sum,x| sum + x }
+
+
+
+
+@input_prob = @blue_input.to_r / @overall_int.to_r
+
+puts @input_prob
+
+@grey_prob = 1/40r*1/39r*1/38r*1/37r*1/36r
+
+
+
 
 end
 
 user_input
+
+#TESTING
